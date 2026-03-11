d82bd4e9a805e852b1353e771e1129a13e8581f7
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-03-11 20:19:12 +0530

    update calulator using function

--- JAVASCRIPT/Calculator/cal.js
+++ JAVASCRIPT/Calculator/cal.js
@@ -1,2 +1,17 @@
-let currentDisplay = '';
-document.querySelector(".display").value = currentDisplay;
+let currentDisplay = document.querySelector(".display");
+
+function press(value) {
+  currentDisplay.value += value
+}
+
+function clearDisplay() {
+  currentDisplay.value = '';
+}
+
+function backspace() {
+  currentDisplay.value = currentDisplay.value.slice(0, -1);
+}
+
+function result() {
+  currentDisplay.value = eval(currentDisplay.value);
+}
